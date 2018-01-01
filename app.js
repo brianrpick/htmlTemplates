2fc26ed9a0ff38fd17fc1cac85178c2f1e42d44d
Brian Pick <brianr.pick@gmail.com>
2017-12-31 16:42:25 -0800

    testing html templates

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+
+app.get("/", function(req, res){
+	res.render("home.ejs");
+});
+
+
+
+app.listen(3000, function(){
+	console.log("Serving app on port 3000");
+});
