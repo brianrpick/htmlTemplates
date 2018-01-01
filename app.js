717cd3acae341711f853b1b59fb64f6fe0802693
Brian Pick <brianr.pick@gmail.com>
2017-12-31 16:55:54 -0800

    passing variables through params

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
 	res.render("home.ejs");
 });
 
+app.get("/found/:thing", function(req, res){
+	var thing = req.params.thing
+	res.render("found.ejs", {passedThing: thing});
+});
 
 
 app.listen(3000, function(){
