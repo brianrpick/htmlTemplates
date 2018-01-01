339b91eb6ff89439fc7cda28773f3c407e21d248
Brian Pick <brianr.pick@gmail.com>
2017-12-31 17:42:44 -0800

    added 'posts' page with loop logic

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get("/found/:thing", function(req, res){
 	res.render("found.ejs", {passedThing: thing});
 });
 
+app.get("/posts", function(req, res){
+	var posts = [
+		{title: "Post 1", author: "Brian"},
+		{title: "Post 2", author: "Sophie"},
+		{title: "Post 3", author: "Candice"},
+		{title: "Post 4", author: "Jen"}
+	];
+	res.render("posts.ejs", {posts: posts});
+	
+})
 
 app.listen(3000, function(){
 	console.log("Serving app on port 3000");
